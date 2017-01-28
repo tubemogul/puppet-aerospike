9254187a56699f3a693c495fd130dabb95ba5389
Joseph Herlant <herlantj@gmail.com>
2017-01-27 19:41:52 -0800

    Rubocop cleanup: Lint/ParenthesesAsGroupedExpression

--- spec/defines/xdr_credentials_file_spec.rb
+++ spec/defines/xdr_credentials_file_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'aerospike::xdr_credentials_file' do
   context 'definition of a credentials for DC1' do
-    let (:title) { 'DC1' }
-    let (:params) {
+    let(:title) { 'DC1' }
+    let(:params) {
       {
         all_xdr_credentials: {"DC1"=>{"username"=>"xdr_user_DC1", "password"=>"xdr_password_DC1"}}
       }
